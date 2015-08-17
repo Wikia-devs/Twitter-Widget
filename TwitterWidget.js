af701f66381b0dea46d89404a967259246850fd3
Nate Watson <natewatson999@gmail.com>
2015-08-17 15:42:07 -0400

    De-minified the code, per request
    See the basic definition.

--- TwitterWidget.js
+++ TwitterWidget.js
@@ -12,7 +12,23 @@
  */
 
 $(".twitter-feed").each(function(index){
-	$(this).append('<scr' + 'ipt>!function(d,s,id){var js, fjs=d.getElementsByTagName(s)[0]; if(!d.getElementById(id)){ js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</scr' + 'ipt>');
+	$(this).append('<scr' + 'ipt>\
+		!function(d,s,id){\
+			var js, fjs=d.getElementsByTagName(s)[0];\
+			if(!d.getElementById(id)){ \
+				js=d.createElement(s);\
+				js.id=id;\
+				js.src="https://platform.twitter.com/widgets.js"; \
+				fjs.parentNode.insertBefore(js,fjs);\
+			}\
+		}(document,"script","twitter-wjs");\
+		</scr' + 'ipt>');
 	var $input = $( this );
-	$(this).prepend('<a class="twitter-timeline" href="' + mw.html.escape($input.attr('href')) + '" data-widget-id="' + mw.html.escape($input.attr('data-widget-id')) + '" width="700" height="1250">Tweets from ' + mw.html.escape($input.attr('source')) + '</a>');
+	$(this).prepend('<a class="twitter-timeline" href="' 
+		+ mw.html.escape($input.attr('href')) 
+		+ '" data-widget-id="' 
+		+ mw.html.escape($input.attr('data-widget-id')) 
+		+ '" width="700" height="1250">Tweets from ' 
+		+ mw.html.escape($input.attr('source')) 
+		+ '</a>');
 });
